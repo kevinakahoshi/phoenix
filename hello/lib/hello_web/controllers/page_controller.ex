b74d86f6899b0239b56775db8be0f4a986eb563d
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2026-01-15 22:41:24 -0800

    Learning about redirects

--- hello/lib/hello_web/controllers/page_controller.ex
+++ hello/lib/hello_web/controllers/page_controller.ex
@@ -4,8 +4,18 @@ defmodule HelloWeb.PageController do
   def index(conn, _params) do
     # render(conn, :home)
     # send_resp(conn, 201, "")
-    conn
-    |> put_resp_content_type("text/plain")
-    |> send_resp(201, "")
+    # conn
+    # |> put_resp_content_type("text/plain")
+    # |> send_resp(201, "")
+
+    # conn
+    # |> put_status(202)
+    # |> render(:home)
+
+    redirect(conn, to: ~p"/redirect_test")
+  end
+
+  def redirect_test(conn) do
+    render(conn, :home)
   end
 end
