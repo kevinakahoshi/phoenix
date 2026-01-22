d5190571ec3ebc7fd039a480e5147102ff794adb
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2026-01-21 22:01:04 -0800

    Working dynamic data in templates

--- hello/lib/hello_web/controllers/page_controller.ex
+++ hello/lib/hello_web/controllers/page_controller.ex
@@ -23,4 +23,8 @@ defmodule HelloWeb.PageController do
     render(conn, :home)
     # redirect(conn, external: "https://elixir-lang.org/")
   end
+
+  def count(conn, _params) do
+    render(conn, :count)
+  end
 end
