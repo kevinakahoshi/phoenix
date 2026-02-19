1f7edc58a69d6c9d3aefad5913fbd2888060032b
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2026-02-18 22:18:16 -0800

    Initial temp

--- hello/lib/hello_web/controllers/thermostat_controller.ex
+++ hello/lib/hello_web/controllers/thermostat_controller.ex
@@ -1,42 +1,9 @@
-defmodule HelloWeb.HelloController do
+defmodule HelloWeb.ThermostatController do
   use HelloWeb, :controller
 
-  def index(conn, _params) do
-    render(conn, :index)
-  end
-
-  def count(conn, _params) do
-    render(conn, :count)
-  end
-
-  def list(conn, _params) do
-    nums = [1, 2, 3, 4, 5]
-
+  def show(conn, %{"initial_temp" => initial_temp}) do
     conn
-    |> assign(:nums, nums)
-    |> render(:list)
-  end
-
-  def conditional(conn, _params) do
-    condition = 5 > 3
-
-    conn
-    |> assign(:condition, condition)
-    |> render(:conditional)
-  end
-
-  def show(conn, %{"messenger" => messenger}) do
-    conn
-    |> assign(:messenger, messenger)
-    |> assign(:receiver, "Dweezil")
+    |> assign(:initial_temp, initial_temp)
     |> render(:show)
-  end
-
-  def boop(conn, %{"message" => message}) do
-    text(conn, "Message: #{message}")
-  end
-
-  def beep(conn, %{"input" => input}) do
-    json(conn, %{input: input, boop: "beep"})
   end
 end
